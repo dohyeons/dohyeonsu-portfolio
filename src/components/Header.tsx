53ccd5002ee1dd8aae0af416a67b451a1282eb23
dohyeons <ddhhss0603@gmail.com>
2023-03-22 02:20:23 +0900

    :lipstick: design: 모바일 nav메뉴 제외 반응형 적용 #5

---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { TbSquareRoundedLetterD } from 'react-icons/tb'
+
+export default function Header() {
+	return (
+		<header className="border-b h-16 flex justify-center">
+			<div className="w-desktopHeader tablet:mx-9 mobile:mx-6 h-full flex items-center justify-between">
+				<button type="button">
+					<TbSquareRoundedLetterD className="w-10 h-10 text-nav hover:cursor-pointer mobile:w-8 mobile:h-8 hover:text-inherit duration-300" />
+				</button>
+				<nav className="">
+					<ul className="flex w-nav justify-between ">
+						<li className="inline-block text-xl text-nav">
+							<span className="">About</span>
+						</li>
+						<li className="inline-block text-xl text-nav">
+							<span>Skills</span>
+						</li>
+						<li className="inline-block text-xl text-nav">
+							<span>Projects</span>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	)
+}
