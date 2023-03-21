6edbb741b63c5e03d40f3dbb8cf9eb1a4cc343cf
dohyeons <ddhhss0603@gmail.com>
2023-03-22 02:32:24 +0900

    :lipstick: design: 색 변화만 duration적용되게 transition-color추가 #5

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
 		<header className="border-b h-16 flex justify-center">
 			<div className="w-desktopHeader tablet:mx-9 mobile:mx-6 h-full flex items-center justify-between">
 				<button type="button">
-					<TbSquareRoundedLetterD className="w-10 h-10 text-nav hover:cursor-pointer mobile:w-8 mobile:h-8 hover:text-inherit duration-300" />
+					<TbSquareRoundedLetterD className="w-10 h-10 text-nav hover:cursor-pointer mobile:w-8 mobile:h-8 hover:text-inherit transition-colors duration-300" />
 				</button>
 				<nav className="">
 					<ul className="flex w-nav justify-between ">
