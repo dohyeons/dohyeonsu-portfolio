bda79c4a3fa7795e83191b147da629eb5fac2c48
dohyeons <ddhhss0603@gmail.com>
2023-03-23 00:42:20 +0900

    :recycle: refactoring: li요소를 map으로 전개 #5

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,23 +2,24 @@
 import { TbSquareRoundedLetterD } from 'react-icons/tb'
 
 export default function Header() {
+	const navMenus: string[] = ['About', 'Skills', 'Projects']
+
 	return (
 		<header className="border-b h-16 flex justify-center">
 			<div className="w-desktopHeader tablet:mx-9 mobile:mx-6 h-full flex items-center justify-between">
 				<button type="button">
 					<TbSquareRoundedLetterD className="w-10 h-10 text-nav hover:cursor-pointer mobile:w-8 mobile:h-8 hover:text-inherit transition-colors duration-300" />
 				</button>
-				<nav className="">
+				<nav>
 					<ul className="flex w-nav justify-between ">
-						<li className="inline-block text-xl text-nav">
-							<span className="">About</span>
-						</li>
-						<li className="inline-block text-xl text-nav">
-							<span>Skills</span>
-						</li>
-						<li className="inline-block text-xl text-nav">
-							<span>Projects</span>
-						</li>
+						{navMenus.map((navMenu, idx) => (
+							<li
+								key={`${idx.toString()}-${navMenu}`}
+								className="inline-block text-xl text-nav"
+							>
+								<span>{navMenu}</span>
+							</li>
+						))}
 					</ul>
 				</nav>
 			</div>
