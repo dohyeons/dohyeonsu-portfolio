d90617f5f6e78a75cbbc279534f65d257f2d0450
dohyeons <ddhhss0603@gmail.com>
2023-03-23 02:46:01 +0900

    :recycle: refactoring: 홈 아이콘 반응형 코드 위치 수정 #5

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
 		<header className="border-b h-16 flex justify-center">
 			<div className="w-desktopHeader headerContainer-reponsive h-full flex items-center justify-between">
 				<button type="button">
-					<TbSquareRoundedLetterD className="w-10 h-10 text-nav hover:cursor-pointer mobile:w-8 mobile:h-8 hover:text-inherit transition-colors duration-300" />
+					<TbSquareRoundedLetterD className="w-10 h-10 text-nav hover:cursor-pointer hover:text-inherit transition-colors duration-300 mobile:w-8 mobile:h-8 " />
 				</button>
 				<nav>
 					<ul className="flex w-nav justify-between ">
