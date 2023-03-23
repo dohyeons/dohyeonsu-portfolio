d586675733195583406eb291ecb522bdc15bf413
dohyeons <ddhhss0603@gmail.com>
2023-03-23 11:13:11 +0900

    :lipstick: design: 버튼 위치 변경 및 nav에 조건부 반응형 적용 #5

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { TbSquareRoundedLetterD } from 'react-icons/tb'
 import { RiStackFill } from 'react-icons/ri'
+import { useState } from 'react'
 
 export default function Header() {
 	const navMenu = ['About', 'Skills', 'Projects']
+	const [isHamburgerClicked, setIsHamburgerClicked] = useState(false)
 
 	return (
 		<header className="border-b h-16 flex justify-center">
@@ -11,11 +13,11 @@ export default function Header() {
 				<div>
 					<TbSquareRoundedLetterD className="w-10 h-10 text-nav hover:cursor-pointer hover:text-inherit transition-colors duration-300 mobile:w-8 mobile:h-8 " />
 				</div>
-				<nav>
-					<button type="button" className="hidden mobile:block">
-						<RiStackFill className="w-7 h-7 text-nav hover:cursor-pointer hover:text-inherit transition-colors duration-300" />
-					</button>
-					<ul className="flex w-nav justify-between mobile:hidden">
+				<button type="button" className="hidden mobile:block">
+					<RiStackFill className="w-7 h-7 text-nav hover:cursor-pointer hover:text-inherit transition-colors duration-300" />
+				</button>
+				<nav className={`${isHamburgerClicked ? 'block' : 'mobile:hidden'}`}>
+					<ul className="flex w-nav justify-between">
 						{navMenu.map((Menu, idx) => (
 							<li
 								key={`${idx.toString()}-${Menu}`}
