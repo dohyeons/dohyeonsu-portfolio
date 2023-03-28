29a3e2cfd782ce2e9df866fcc74e839d11f67112
dohyeons <ddhhss0603@gmail.com>
2023-03-29 02:50:57 +0900

    :lipstick: design: About Me 컴포넌트 반응형 완료 #6

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,34 +2,37 @@ import Image from 'next/image'
 import Profile from 'public/Do-hyeonsu_profile.jpeg'
 
 export default function AboutMe() {
+	const introduction = [
+		'안녕하세요 UI/UX에 관심이 많은 프론트엔드 개발자 도현수입니다',
+		'자기소개 자기소개 자기소개 자기소개 자기소개자기소개 자기소개 자기소개 자기소개 자기소개',
+		'자기소개 자기소개 자기소개 자기소개 자기소개자기소개 자기소개 자기소개 자기소개 자기소개',
+		'자기소개 자기소개 자기소개 자기소개 자기소개자기소개 자기소개 자기소개 자기소개 자기소개',
+		'자기소개 자기소개 자기소개 자기소개 자기소개자기소개 자기소개 자기소개 자기소개 자기소개',
+	]
 	return (
-		<div className="flex justify-center">
-			<section className="flex-col flex w-desktopHeader justify-center content-center p-16">
-				<h2 className="text-center text-5xl font-black">ABOUT ME</h2>
-				<article className="flex justify-between">
+		<section className="flex justify-center bg-oddBg">
+			<div className="flex flex-col space-y-7 w-desktopHeader items-center p-16">
+				<h2 className="text-5xl font-black  mobile:text-3xl">ABOUT ME</h2>
+				<article className="flex items-center space-x-16 tablet:flex-col tablet:space-y-14 tablet:space-x-0 mobile:flex-col mobile:space-y-14 mobile:space-x-0">
 					<Image
 						src={Profile}
 						alt="Profile Image"
 						width={260}
 						height={260}
-						className=" rounded-md"
+						className="rounded-md"
 					/>
-					<div className="flex-col inline-flex justify-center">
-						<p>
-							자기소개 자기소개 자기소개 자기소개 자기소개 자기소개 자기소개
-						</p>
-						<p>
-							자기소개 자기소개 자기소개 자기소개 자기소개 자기소개 자기소개
-						</p>
-						<p>
-							자기소개 자기소개 자기소개 자기소개 자기소개 자기소개 자기소개
-						</p>
-						<p>
-							자기소개 자기소개 자기소개 자기소개 자기소개 자기소개 자기소개
-						</p>
+					<div className="flex-col h-fit inline-flex tablet:items-center mobile:items-center">
+						{introduction.map((el, idx) => (
+							<p
+								key={`${idx.toString()}-${el}`}
+								className="mobile:text-xs text-nav"
+							>
+								{el}
+							</p>
+						))}
 					</div>
 				</article>
-			</section>
-		</div>
+			</div>
+		</section>
 	)
 }
