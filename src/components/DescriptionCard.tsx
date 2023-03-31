0fefd2801fffa0f11e44c2711c89e9af42a28400
dohyeons <ddhhss0603@gmail.com>
2023-04-01 00:31:52 +0900

    :lipstick: design: DescriptionCard 컴포넌트 동적 스타일링 적용 # 12

--- src/components/DescriptionCard.tsx
+++ src/components/DescriptionCard.tsx
@@ -1,13 +1,21 @@
 import { ReactNode } from 'react'
 
-export default function DescriptionCard({ children }: { children: ReactNode }) {
+interface DescriptionCardProps {
+	children: ReactNode
+	size: string
+}
+export default function DescriptionCard({
+	children,
+	size,
+}: DescriptionCardProps) {
+	const sizeVariants: { [key in DescriptionCardProps['size']]: string } = {
+		contactChannel: 'w-[385px] h-[234px] py-6',
+	}
 	return (
-		<section>
-			<h2>CONTACT</h2>
-			<article>
-				<p>Phone: 010-9936-9661</p>
-				<p>E-mail: dohyeonsu63@gmail.com</p>
-			</article>
-		</section>
+		<article
+			className={`${sizeVariants[size]} bg-white rounded-[20px] drop-shadow-cardShadow flex flex-col justify-between items-center`}
+		>
+			{children}
+		</article>
 	)
 }
