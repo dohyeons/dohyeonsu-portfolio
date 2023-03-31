5cb37c022d398ea3698dc7b1093e3eee4cf23510
dohyeons <ddhhss0603@gmail.com>
2023-03-31 23:29:33 +0900

    :lipstick: design:  ContactChannel 카드 데스크탑 스타일링 완료 #12

--- src/components/ContactChannel.tsx
+++ src/components/ContactChannel.tsx
@@ -1,10 +1,10 @@
 export default function ContactChannel() {
 	return (
 		<section className="flex justify-center px-[22px] bg-evenBg">
-			<div className=" w-[890px] h-[268px] border border-green-600 flex justify-between">
-				<article className="bg-white w-[385px] h-[268px] rounded-[20px] placeholder drop-shadow-cardShadow">
-					<h2>CONTACT</h2>
-					<ul>
+			<div className=" w-[890px] h-[268px] border border-green-600 flex justify-between items-center">
+				<article className="bg-white w-[385px] h-[234px] rounded-[20px] placeholder drop-shadow-cardShadow py-6 flex flex-col justify-between items-center">
+					<h2 className="text-5xl font-black  mobile:text-3xl">CONTACT</h2>
+					<ul className="flex flex-col justify-between h-[90px] text-nav">
 						<li>Phone: 010-9936-9771</li>
 						<li>E-mail: dohyeonsu63@gmail.com</li>
 					</ul>
