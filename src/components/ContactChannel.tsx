6ec5e8345833b3ed135351188de34a01c258edce
dohyeons <ddhhss0603@gmail.com>
2023-03-31 23:18:37 +0900

    :lipstick: design: ContcatChannel 박스 생성 #12

--- src/components/ContactChannel.tsx
+++ src/components/ContactChannel.tsx
@@ -1,5 +1,15 @@
 export default function ContactChannel() {
 	return (
-		<section className="flex justify-center bg-evenBg"> 컨텍트 채널</section>
+		<section className="flex justify-center px-[22px] bg-evenBg">
+			<div className=" w-[890px] h-[268px] border border-green-600 flex justify-between">
+				<article className="bg-white w-[385px] h-[268px] rounded-[20px] placeholder drop-shadow-cardShadow">
+					<h2>CONTACT</h2>
+					<ul>
+						<li>Phone: 010-9936-9771</li>
+						<li>E-mail: dohyeonsu63@gmail.com</li>
+					</ul>
+				</article>
+			</div>
+		</section>
 	)
 }
