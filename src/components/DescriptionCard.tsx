e6d450ad3dc39f3966a284f0f3baf9c4202e6609
dohyeons <ddhhss0603@gmail.com>
2023-03-31 22:30:24 +0900

    :lipstick: design: DescriptionCard 컴포넌트 초안 구현 #12

---
+++ src/components/DescriptionCard.tsx
@@ -0,0 +1,11 @@
+export default function DescriptionCard() {
+	return (
+		<section>
+			<h2>CONTACT</h2>
+			<article>
+				<p>Phone: 010-9936-9661</p>
+				<p>E-mail: dohyeonsu63@gmail.com</p>
+			</article>
+		</section>
+	)
+}
