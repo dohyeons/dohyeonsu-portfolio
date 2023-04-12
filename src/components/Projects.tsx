79c3321f67b0b0f47a0497b881081edbbdd976df
dohyeons <ddhhss0603@gmail.com>
2023-04-12 17:34:59 +0900

    :lipstick: design: section 초기 구현 (반응형 x) #16

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,9 @@
 export default function Projects() {
 	return (
-		<h2 className="text-5xl font-black text-white drop-shadow-headingShadow mobile:text-3xl">
-			Projects
-		</h2>
+		<section className="flex flex-col items-center bg-evenBg px-9 space-y-[70px]">
+			<h2 className="text-5xl font-black text-white drop-shadow-headingShadow mobile:text-3xl">
+				Projects
+			</h2>
+		</section>
 	)
 }
