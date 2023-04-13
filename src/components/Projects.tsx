53a5b4fc435f252f9bb37ed8b990c157ba972a4b
dohyeons <ddhhss0603@gmail.com>
2023-04-13 20:19:05 +0900

    :lipstick: design: Projects 섹션 기본 구조 구현 #16

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+import projectsImage from 'public/Pillivery.png'
 import DescriptionCard from './DescriptionCard'
 
 export default function Projects() {
@@ -8,11 +10,24 @@ export default function Projects() {
 			</h2>
 			<div>
 				<DescriptionCard size="projects">
-					<div className="flex flex-col h-[76px] items-center justify-between">
+					<div className="flex flex-col h-[76px] items-center justify-between border border-black">
 						<h3 className="text-4xl font-black mobile:text-2xl">Pillivery</h3>
 						<p className="text-[20px] font-normal text-subtitle">
 							2022.11.08 ~ 2022.12.07 코드스테이츠 파이널 프로젝트 (7인/4주)
 						</p>
+					</div>
+					<div className="flex max-w-[860px] w-full space-x-[20px] border border-red-400">
+						<Image
+							alt="프로젝트 이미지"
+							src={projectsImage}
+							width={460}
+							height={460}
+						/>
+						<div className="max-w-[405px] w-full border border-lime-300">
+							<p className="font-semibold">
+								프로젝트 설명입니다.프로젝트 설명입니다.프로젝트
+							</p>
+						</div>
 					</div>
 				</DescriptionCard>
 			</div>
