3bbe78b4eeab5c13087bb6015ae06c35e03f880f
dohyeons <ddhhss0603@gmail.com>
2023-04-24 23:47:40 +0900

    :lipstick: design:프로젝트 섹션  모바일 버전에서 좌우 여백 조절 #16

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -21,7 +21,7 @@ export default function Projects() {
 	]
 
 	return (
-		<section className="flex flex-col items-center bg-evenBg px-9 space-y-[70px] py-[70px]">
+		<section className="flex flex-col items-center bg-evenBg px-9 mobile:px-8 space-y-[70px] py-[70px]">
 			<h2 className="text-5xl font-black text-white drop-shadow-headingShadow mobile:text-3xl">
 				Projects
 			</h2>
