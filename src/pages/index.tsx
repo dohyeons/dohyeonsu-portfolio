52f552d4cbe07aa128dacf0a90bb6f6f87280dac
dohyeons <ddhhss0603@gmail.com>
2023-04-26 23:07:03 +0900

    :sparkles: feat: Head로 title 설정 #24

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,14 @@ import ContactChannel from '@/components/ContactChannel'
 import Skills from '@/components/Skills'
 import Projects from '@/components/Projects'
 import Footer from '@/components/Footer'
+import Head from 'next/head'
 
 export default function Home() {
 	return (
 		<>
+			<Head>
+				<title> FE 개발자 도현수 포트폴리오</title>
+			</Head>
 			<Header />
 			<AboutMe />
 			<ContactChannel />
