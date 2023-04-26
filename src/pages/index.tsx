559798843a34fbba61013cabc30a0dab2527e40b
dohyeons <ddhhss0603@gmail.com>
2023-04-26 23:47:52 +0900

    :sparkles: feat: Title 컴포넌트 구현 #24

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,19 +4,12 @@ import ContactChannel from '@/components/ContactChannel'
 import Skills from '@/components/Skills'
 import Projects from '@/components/Projects'
 import Footer from '@/components/Footer'
-import Head from 'next/head'
+import Title from '@/components/Title'
 
 export default function Home() {
 	return (
 		<>
-			<Head>
-				<title> FE 개발자 도현수 포트폴리오</title>
-				<meta
-					property="og:title"
-					content="FE 개발자 도현수 포트폴리오"
-					key="title"
-				/>
-			</Head>
+			<Title />
 			<Header />
 			<AboutMe />
 			<ContactChannel />
