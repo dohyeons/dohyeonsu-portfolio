0e569f4aa055cfdcc233dd11d2df5364d820c1bb
dohyeons <ddhhss0603@gmail.com>
2023-04-26 23:43:10 +0900

    :sparkles: feat: meta 태그 추가 #24

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,11 @@ export default function Home() {
 		<>
 			<Head>
 				<title> FE 개발자 도현수 포트폴리오</title>
+				<meta
+					property="og:title"
+					content="FE 개발자 도현수 포트폴리오"
+					key="title"
+				/>
 			</Head>
 			<Header />
 			<AboutMe />
