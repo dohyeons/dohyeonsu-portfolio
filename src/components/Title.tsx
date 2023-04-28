9f18a7a38fbb536404524a58d4a9253b418c768d
dohyeons <ddhhss0603@gmail.com>
2023-04-29 04:42:24 +0900

    meta 태그 설정

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -5,10 +5,19 @@ export default function Title() {
 		<Head>
 			<title> FE 개발자 도현수 포트폴리오</title>
 			<meta
+				name="description"
+				content="UX에 관심이 많은 프론트엔드 개발자 도현수 입니다."
+			/>
+			<meta
 				property="og:title"
 				content="FE 개발자 도현수 포트폴리오"
 				key="title"
 			/>
+			<meta
+				property="og:url"
+				content="https://dohyeonsu-portfolio.vercel.app/"
+			/>
+			<meta property="og:type" content="website" />
 			<meta
 				property="og:description"
 				content="UX에 관심이 많은 프론트엔드 개발자 도현수 입니다."
