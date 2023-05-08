6dd0b3fd59162a5d6a57f37e5f1847ea0c98b5cc
dohyeons <ddhhss0603@gmail.com>
2023-05-09 00:53:05 +0900

    :green_heart: chore: ProjectButton 컴포넌트 리팩터링 #26

--- src/components/sections/Projects/ProjectButton.tsx
+++ src/components/sections/Projects/ProjectButton.tsx
@@ -12,26 +12,30 @@ export default function ProjectButton({
 	buttonName,
 	address,
 }: ProjectButtonProps) {
-	const buttonType: { [key in ProjectButtonProps['buttonName']]: string[] } = {
+	const buttonType: {
+		[key in ProjectButtonProps['buttonName']]: [string, React.ReactElement]
+	} = {
 		github: [
 			'bg-githubButton pl-[10px] pr-3 mobile:pr-1 hover:bg-hoveredGithubButton',
+			<AiFillGithub key="github" className="ProjectButton" />,
 		],
 		blog: [
 			'bg-blogButton pl-[10px] pr-6 mobile:pr-3 hover:bg-hoveredBlogButton',
+			<BiBookBookmark key="blog" className="ProjectButton" />,
 		],
 		visit: [
 			'bg-visitButton pl-[10px] pr-7 mobile:pr-4 hover:bg-hoveredVisitButton',
+			<TiLocation key="visit" className="ProjectButton" />,
 		],
 	}
+
 	return (
 		<Link
 			href={address}
 			className={`flex ${buttonType[buttonName][0]} items-center mobile:text-xs mobile:w-full mobile:h-10 w-28 h-12 rounded-lg text-white text-sm font-bold transition-colors duration-300`}
 		>
 			<div className="flex items-center w-full justify-center gap-2">
-				{buttonName === 'github' && <AiFillGithub className="ProjectButton" />}
-				{buttonName === 'blog' && <BiBookBookmark className="ProjectButton" />}
-				{buttonName === 'visit' && <TiLocation className="ProjectButton" />}
+				{buttonType[buttonName][1]}
 				<p>{buttonName.toUpperCase()}</p>
 			</div>
 		</Link>
