a5f017b2df122fe8bdee9d297152975c13244a71
dohyeons <ddhhss0603@gmail.com>
2023-05-09 00:21:38 +0900

    :green_heart: chore: SkillCardGroup 컴포넌트 분리 #26

--- src/components/sections/Skills/Index.tsx
+++ src/components/sections/Skills/Index.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image'
-import { skillLogo } from '@/asset/Contants'
 import SectionHeading from '@/components/common/SectionHeading'
-import DescriptionCard from '../../common/DescriptionCard'
+import SkillCardGroup from './SkillCardGroup'
 
 export default function Skills() {
 	return (
@@ -10,22 +8,7 @@ export default function Skills() {
 			className="flex flex-col items-center bg-oddBg px-9 tablet:px-[158px] mobile:px-0 py-[70px] mobile:py-[35px] space-y-[70px] mobile:space-y-[35px]"
 		>
 			<SectionHeading heading="SKILLS" />
-			<div className="w-[831px] mobile:w-full tablet:w-[520px] gap-4 mobile:max-w-[230px] flex flex-wrap justify-evenly">
-				{skillLogo.map((logo, idx) => (
-					<DescriptionCard size="skills" key={logo + idx.toString()}>
-						<div className="w-32 h-32 mobile:w-[44px] mobile:h-[44px] flex items-center">
-							<Image
-								src={logo}
-								alt="스킬 로고"
-								width={128}
-								height={128}
-								className="mobile:w-[44px] mobile:h-[44px]"
-							/>
-						</div>
-						<div className=" text-sm mobile:hidden">{logo.slice(0, -4)}</div>
-					</DescriptionCard>
-				))}
-			</div>
+			<SkillCardGroup />
 		</section>
 	)
 }
